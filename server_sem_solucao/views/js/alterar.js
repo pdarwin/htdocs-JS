9e4dac051b259aec6b8d0fccfbc2dd4e51cc7a31
pdarwin <46503478+pdarwin@users.noreply.github.com>
2022-03-04 13:59:18 +0000

    act

--- server_sem_solucao/views/js/alterar.js
+++ server_sem_solucao/views/js/alterar.js
@@ -3,22 +3,40 @@ document.getElementById('change').addEventListener("click", function() {
     document.getElementById('modal').style.display = 'none';
     var name = document.getElementById('i-name').value;
     var password = document.getElementById('i-password').value;
-    var job = document.getElementById('i-job').value;
     var id = document.getElementById('i-id').value;
 
-    var data = 'password=' + password + '&id=' + id + '&name=' + name + '&profession=' + job;
+    if (name != "" && password != "")
+    {
+        var data = 'password=' + password + '&id=' + id + '&name=' + name;
 
 
-    const xhttp = new XMLHttpRequest();
-    xhttp.open('POST', "/setUser");
-    xhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+        const xhttp = new XMLHttpRequest();
+        xhttp.open('POST', "/setUser");
+        xhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+    
+        xhttp.onload = function() {
+            console.log(this.responseText);
+        }
+    
+        xhttp.send(data);
 
-    xhttp.onload = function() {
-        console.log(this.responseText);
+        document.location.reload();
+    
+    }
+    
+    if (name == "")
+    {   
+        document.getElementById('i-name').style.border=1;
+        document.getElementById('i-name').style.borderColor="red";  
+        document.getElementById('modal').style.display = "block";
+    }
+    
+    if (password == "")
+    {
+        document.getElementById('i-password').style.border=1;
+        document.getElementById('i-password').style.borderColor="red";
+        document.getElementById('modal').style.display = "block";
     }
 
-    xhttp.send(data);
-
-    document.location.reload();
 
 });
--- server_sem_solucao/views/js/pedido.js
+++ server_sem_solucao/views/js/pedido.js
@@ -1,46 +1,107 @@
 function loadDoc() {
-    const xhttp = new XMLHttpRequest();
-    xhttp.onload = function() 
-    {
+  
+  const xhttp = new XMLHttpRequest();
+  xhttp.onload = function () {
 
-      let users = JSON.parse (this.responseText).user;
-      
-      let table = document.getElementById("table");
+    let users = JSON.parse(this.responseText).user;
 
-      users.forEach (function (element, index)
-      {
-        let row = table.insertRow (index + 1);
-        insertCell(row, 0, "<button id='" + index + "'>Detalhe</button>");
-        insertCell (row, 0, element.id);
-        insertCell (row, 0, element.profession);
-        insertCell (row, 0, element.password);
-        insertCell (row, 0, element.name);
-      })
+    buildTable(users);
 
-      addEventButton(users);
+    addEventButton(users);
+    addEventRefresh();
+    addEventShowRows(users);
 
-    }
-    xhttp.open("GET", "/listUsers");
-    xhttp.send();
   }
+  xhttp.open("GET", "/listUsers");
+  xhttp.send();
+}
 
-  function insertCell(row, index, value) {
-    var cell1 = row.insertCell(index);
-    cell1.innerHTML = value;
+function buildTable(users) {
+  let table = document.getElementById("table");
+
+  var today = new Date();
+
+  users.forEach(function (element, index) {
+    let row = table.insertRow(index + 1);
+    insertCell(row, 0, "<button id='" + index + "'>Detalhe</button>");
+    insertCell(row, 0, today.getDate() + "/" + today.getMonth() + "/" + today.getFullYear());
+    insertCell(row, 0, element.id);
+    insertCell(row, 0, GetPwdHash(element.password));
+    insertCell(row, 0, element.profession);
+    insertCell(row, 0, element.name);
+    row.cells[0].style.backgroundColor = "cadetblue";
+    row.cells[0].style.textTransform = "uppercase";
+
+  })
+}
+
+function insertCell(row, index, value) {
+  var cell1 = row.insertCell(index);
+  cell1.innerHTML = value;
+}
+
+function GetPwdHash(pwd) {
+  length = pwd.length;
+  hash = "";
+  for (i = 0; i < length; i++) {
+    hash += "*";
+  }
+  return hash;
+}
+
+function addEventRefresh() {
+  let btn = document.getElementById("refresh");
+  btn.addEventListener("click", function () {
+    document.location.reload();
+  });
+}
+
+function addEventShowRows(users)
+{
+  
+  for (i = 0; i < 3; i++) 
+  {
+    bname = "b" + i;
+    let btn = document.getElementById(bname);
+    btn.addEventListener("click", function () 
+    {
+      showOneLineTable (users, i);
+    });
+  };
+}
+
+function showOneLineTable (users, index)
+{
+
+  let table = document.getElementById("table");
+
+  element = users[index];
+  let row = table.insertRow(index);
+  insertCell(row, 0, "<button id='" + index + "'>Detalhe</button>");
+  insertCell(row, 0, today.getDate() + "/" + today.getMonth() + "/" + today.getFullYear());
+  insertCell(row, 0, element.id);
+  insertCell(row, 0, GetPwdHash(element.password));
+  insertCell(row, 0, element.profession);
+  insertCell(row, 0, element.name);
+  row.cells[0].style.backgroundColor = "cadetblue";
+  row.cells[0].style.textTransform = "uppercase";
+
+  xhttp.open("GET", "/listUsers");
+  xhttp.send();
+
 }
 
 function addEventButton(users) {
-    users.forEach(function(element, index) {
-        let btn = document.getElementById(index);
-        btn.addEventListener("click", function() {
-            document.getElementById('modal').style.display = 'block'
-            document.getElementById('i-name').value = element.name;
-            document.getElementById('i-password').value = element.password;
-            document.getElementById('i-job').value = element.profession;
-            document.getElementById('i-id').value = element.id;
-        });
+  users.forEach(function (element, index) {
+    let btn = document.getElementById(index);
+    btn.addEventListener("click", function () {
+      document.getElementById('modal').style.display = 'block'
+      document.getElementById('i-name').value = element.name;
+      document.getElementById('i-password').value = element.password;
+      document.getElementById('i-id').value = element.id;
     });
-  
-  }
+  });
 
-  loadDoc();
+}
+
+loadDoc();
